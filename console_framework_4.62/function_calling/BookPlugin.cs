4401d92645c902ef852056db0473c7520b710874
Loïc Carrère <loic.carrere@gmail.com>
2024-08-12 15:24:08 +0200

    added function calling demo

---
+++ console_framework_4.62/function_calling/BookPlugin.cs
@@ -0,0 +1,167 @@
+﻿using LMKit.FunctionCalling;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace function_calling
+{
+    internal class BookPlugin
+    {
+        private readonly HttpClient client = new HttpClient();
+
+        [LMFunction("GetAvailableBookCountByAuthor", "Retrieves the count of books available by a specified author.")]
+        public async Task<int> GetAvailableBookCountByAuthor([Description("The name of the author whose available books are being counted.")] string author)
+        {
+            string url = "https://openlibrary.org/search.json?author=" + Uri.EscapeDataString(author); //api ref: https://openlibrary.org/dev/docs/api/search
+
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+
+            Stream content = await response.Content.ReadAsStreamAsync();
+
+            JsonDocument jsonDoc = JsonDocument.Parse(content);
+
+            JsonElement numElement = jsonDoc.RootElement.GetProperty("num_found");
+
+            if (numElement.TryGetInt32(out int count))
+            {
+                return count;
+            }
+
+            return 0;
+        }
+
+        [LMFunction("GetBookInfo", "Retrieves detailed information about a specified book.")]
+        public async Task<string> GetBookInfo([Description("The title of the book to retrieve information for.")] string title)
+        {
+            string url = "https://openlibrary.org/search.json?title=" + Uri.EscapeDataString(title); //api ref: https://openlibrary.org/dev/docs/api/search
+
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+
+            Stream content = await response.Content.ReadAsStreamAsync();
+
+            JsonDocument jsonDoc = JsonDocument.Parse(content);
+            var docs = jsonDoc.RootElement.GetProperty("docs");
+            List<JsonElement> docsArray = docs.EnumerateArray()
+                                              .Cast<JsonElement>()
+                                              .ToList();
+
+            string publish_year = "unknown";
+            string author = "unknown";
+            string number_of_pages_median = "unknown";
+
+            if (docsArray.Count > 0)
+            {
+                JsonElement titleElement = docsArray[0].GetProperty("title");
+
+                title = titleElement.GetString();
+
+                JsonElement publishYearElement = docsArray[0].GetProperty("publish_year");
+                publish_year = publishYearElement[0].GetInt32().ToString();
+
+                JsonElement pageCountMedElement = docsArray[0].GetProperty("number_of_pages_median");
+                number_of_pages_median = publishYearElement[0].GetInt32().ToString();
+
+                List<JsonElement> authorNameElement = docsArray[0].GetProperty("author_name").EnumerateArray()
+                                                                                .Cast<JsonElement>()
+                                                                                .ToList();
+                if (authorNameElement.Count > 0)
+                {
+                    author = authorNameElement[0].GetString();
+                }
+            }
+
+            return "First publish year: " + publish_year + "\n" +
+                   "Author: " + author + "\n" +
+                   "Number of pages median: " + number_of_pages_median;
+        }
+
+        [LMFunction("GetLastBookFromAuthor", "Retrieves detailed information about the most recent book by a specified author.")]
+        public async Task<string> GetLastBookFromAuthor([Description("The name of the author whose latest book's information is being retrieved.")] string author)
+        {
+            string url = "https://openlibrary.org/search.json?author=" + Uri.EscapeDataString(author) + "&sort=new"; //api ref: https://openlibrary.org/dev/docs/api/search
+
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+
+            Stream content = await response.Content.ReadAsStreamAsync();
+
+            JsonDocument jsonDoc = JsonDocument.Parse(content);
+
+            var docs = jsonDoc.RootElement.GetProperty("docs");
+            List<JsonElement> docsArray = docs.EnumerateArray()
+                                                .Cast<JsonElement>()
+                                                .ToList();
+
+            string title = "unknown";
+            string publish_year = "unknown";
+
+            if (docsArray.Count > 0)
+            {
+                JsonElement titleElement = docsArray[0].GetProperty("title");
+
+                title = titleElement.GetString();
+
+                List<JsonElement> publishYear = docsArray[0].GetProperty("publish_year").EnumerateArray()
+                                                                                 .Cast<JsonElement>()
+                                                                                 .ToList();
+                if (publishYear.Count > 0)
+                {
+                    publish_year = publishYear[0].GetInt32().ToString();
+                }
+            }
+
+            return title + ", publish year: " + publish_year;
+        }
+
+
+        [LMFunction("GetBookAuthor", "Retrieves the author's name for a specified book.")]
+        public async Task<string> GetBookAuthor([Description("The title of the book to retrieve the author's name for.")] string book)
+        {
+            string url = "https://openlibrary.org/search.json?q=" + Uri.EscapeDataString(book); //api ref: https://openlibrary.org/dev/docs/api/search
+
+
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+
+            Stream content = await response.Content.ReadAsStreamAsync();
+
+            JsonDocument jsonDoc = JsonDocument.Parse(content);
+
+            var docs = jsonDoc.RootElement.GetProperty("docs");
+            List<JsonElement> docsArray = docs.EnumerateArray()
+                                                .Cast<JsonElement>()
+                                                .ToList();
+
+            if (docsArray.Count > 0)
+            {
+                JsonElement authorElement = docsArray[0].GetProperty("author_name");
+
+                List<JsonElement> authorArray = authorElement.EnumerateArray()
+                                                             .Cast<JsonElement>()
+                                                             .ToList();
+                if (authorArray.Count > 0)
+                {
+                    string author = authorArray[0].GetString();
+
+                    if (author != null)
+                    {
+                        return author;
+                    }
+                }
+            }
+
+            return "unknown";
+        }
+    }
+}
---
+++ console_net6/function_calling/Program.cs
@@ -0,0 +1,156 @@
+﻿using LMKit.FunctionCalling;
+using LMKit.Model;
+using System.Text;
+
+namespace function_calling
+{
+    internal class Program
+    {
+        static readonly string DEFAULT_LLAMA3_1_8B_MODEL_PATH = @"https://huggingface.co/lm-kit/llama-3.1-8b-instruct-gguf/resolve/main/Llama-3.1-8B-Instruct-Q4_K_M.gguf";
+        static readonly string DEFAULT_GEMMA2_9B_MODEL_PATH = @"https://huggingface.co/lm-kit/gemma-2-9b-gguf/resolve/main/gemma-2-9B-Q4_K_M.gguf";
+        static readonly string DEFAULT_PHI3_MINI_3_8B_MODEL_PATH = @"https://huggingface.co/lm-kit/phi-3.1-mini-4k-3.8b-instruct-gguf/resolve/main/Phi-3.1-mini-4k-Instruct-Q4_K_M.gguf";
+        static readonly string DEFAULT_QWEN2_7B_MODEL_PATH = @"https://huggingface.co/lm-kit/qwen-2-7b-instruct-gguf/resolve/main/Qwen-2-7B-Instruct-Q4_K_M.gguf";
+        static readonly string DEFAULT_MISTRAL_NEMO_12_2B_MODEL_PATH = @"https://huggingface.co/lm-kit/mistral-nemo-2407-12.2b-instruct-gguf/resolve/main/Mistral-Nemo-2407-12.2B-Instruct-Q4_K_M.gguf";
+        static bool _isDownloading;
+
+        private static bool ModelDownloadingProgress(string path, long? contentLength, long bytesRead)
+        {
+            _isDownloading = true;
+            if (contentLength.HasValue)
+            {
+                double progressPercentage = Math.Round((double)bytesRead / contentLength.Value * 100, 2);
+                Console.Write($"\rDownloading model {progressPercentage:0.00}%");
+            }
+            else
+            {
+                Console.Write($"\rDownloading model {bytesRead} bytes");
+            }
+
+            return true;
+        }
+
+        private static bool ModelLoadingProgress(float progress)
+        {
+            if (_isDownloading)
+            {
+                Console.Clear();
+                _isDownloading = false;
+            }
+
+            Console.Write($"\rLoading model {Math.Round(progress * 100)}%");
+
+            return true;
+        }
+
+        private static void Main(string[] args)
+        {
+            LMKit.Licensing.LicenseManager.SetLicenseKey(""); //set an optional license key here if available.
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Clear();
+            Console.WriteLine("Please select the model you want to use:\n");
+            Console.WriteLine("0 - Mistral Nemo 2407 12.2B (requires approximately 7.7 GB of VRAM)");
+            Console.WriteLine("1 - Meta Llama 3.1 8B (requires approximately 6 GB of VRAM)");
+            Console.WriteLine("2 - Google Gemma2 9B Medium (requires approximately 7 GB of VRAM)");
+            Console.WriteLine("3 - Microsoft Phi-3 3.82B Mini (requires approximately 3.3 GB of VRAM)");
+            Console.WriteLine("4 - Alibaba Qwen-2 7.6B (requires approximately 5.6 GB of VRAM)");
+            Console.Write("Other entry: A custom model URI\n\n> ");
+
+            string input = Console.ReadLine();
+            string modelLink;
+
+            switch (input.Trim())
+            {
+                case "0":
+                    modelLink = DEFAULT_MISTRAL_NEMO_12_2B_MODEL_PATH;
+                    break;
+                case "1":
+                    modelLink = DEFAULT_LLAMA3_1_8B_MODEL_PATH;
+                    break;
+                case "2":
+                    modelLink = DEFAULT_GEMMA2_9B_MODEL_PATH;
+                    break;
+                case "3":
+                    modelLink = DEFAULT_PHI3_MINI_3_8B_MODEL_PATH;
+                    break;
+                case "4":
+                    modelLink = DEFAULT_QWEN2_7B_MODEL_PATH;
+                    break;
+                default:
+                    modelLink = input.Trim().Trim('"');
+                    break;
+            }
+
+            //Loading model
+            Uri modelUri = new Uri(modelLink);
+            LLM model = new LLM(modelUri,
+                                     downloadingProgress: ModelDownloadingProgress,
+                                     loadingProgress: ModelLoadingProgress);
+
+            Console.Clear();
+            ShowInfo();
+            SingleFunctionCall functionCalling = new SingleFunctionCall(model)
+            {
+                InvokeFunctions = true
+            };
+
+
+            functionCalling.BeforeMethodInvoke += FunctionCalling_BeforeMethodInvoke;
+
+            functionCalling.ImportFunctions<BookPlugin>();
+
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("\n\nType your query: ");
+                Console.ResetColor();
+
+                string prompt = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(prompt))
+                {
+                    break;
+                }
+
+                FunctionCallResult callResult = functionCalling.Submit(prompt);
+
+
+                if (callResult.Method == null)
+                {
+                    Console.WriteLine("> No function has been called");
+                }
+                else
+                {
+                    Console.WriteLine($"Result: {callResult.Result.ToString()}");
+                }
+            }
+
+            Console.WriteLine("The session. Press any key to exit the application.");
+            _ = Console.ReadKey();
+        }
+
+        private static void FunctionCalling_BeforeMethodInvoke(object sender, LMKit.FunctionCalling.Events.BeforeMethodInvokeEventArgs e)
+        {
+            Console.WriteLine("Invoking method " + e.MethodInfo.Name + "...");
+        }
+
+        private static void ShowInfo()
+        {
+            Console.WriteLine("-- Welcome to the  Open Library Search --");
+            Console.WriteLine("\nSupported requests are:");
+            Console.WriteLine("- Retrieves the count of books available by a specified author.");
+            Console.WriteLine("- Retrieves the author's name for a specified book.");
+            Console.WriteLine("- Retrieves detailed information about a specified book.");
+            Console.WriteLine("- Retrieves detailed information about the most recent book by a specified author.");
+
+            Console.WriteLine("\nYou can also ask questions like 'Who wrote The Lord of the Rings?' or 'Give me details about The Lord of the Rings.'\n");
+        }
+
+        private static void Chat_AfterTextCompletion(object sender, LMKit.TextGeneration.Events.AfterTextCompletionEventArgs e)
+        {
+            Console.Write(e.Text);
+        }
+    }
+}
