172cd7ca97f18234a83e6135a18c5411e4cd8e17
Loïc Carrère <loic.carrere@gmail.com>
2025-02-26 20:39:33 +0100

    added a couple of new demos

---
+++ console_net6/text_summarizer_from_image/Program.cs
@@ -0,0 +1,125 @@
+﻿using LMKit.Data;
+using LMKit.Model;
+using LMKit.TextGeneration;
+using System.Diagnostics;
+
+namespace text_summarizer_from_image
+{
+    internal class Program
+    {
+
+        static bool _isDownloading;
+
+        private static bool ModelDownloadingProgress(string path, long? contentLength, long bytesRead)
+        {
+            _isDownloading = true;
+            if (contentLength.HasValue)
+            {
+                double progressPercentage = Math.Round((double)bytesRead / contentLength.Value * 100, 2);
+                Console.Write($"\rDownloading model {progressPercentage:0.00}%");
+            }
+            else
+            {
+                Console.Write($"\rDownloading model {bytesRead} bytes");
+            }
+
+            return true;
+        }
+
+        private static bool ModelLoadingProgress(float progress)
+        {
+            if (_isDownloading)
+            {
+                Console.Clear();
+                _isDownloading = false;
+            }
+
+            Console.Write($"\rLoading model {Math.Round(progress * 100)}%");
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            // Set an optional license key here if available. 
+            // A free community license can be obtained from: https://lm-kit.com/products/community-edition/
+            LMKit.Licensing.LicenseManager.SetLicenseKey("");
+
+            Console.Clear();
+            Console.WriteLine("Please select the model you want to use:\n");
+            Console.WriteLine("0 - Alibaba Qwen 2 Vision 2.2B (requires approximately 3.2 GB of VRAM)");
+            Console.WriteLine("1 - Alibaba Qwen 2 Vision 8.3B (requires approximately 6.4 GB of VRAM)");
+            Console.WriteLine("2 - OpenBMB MiniCPM o 2.6 Vision 8.1B (requires approximately 5.7 GB of VRAM)");
+
+
+            Console.Write("Other entry: A custom model URI\n\n> ");
+
+            string input = Console.ReadLine();
+            string modelLink;
+
+            switch (input.Trim())
+            {
+                case "0":
+                    modelLink = ModelCard.GetPredefinedModelCardByModelID("qwen2-vl:2b").ModelUri.ToString();
+                    break;
+                case "1":
+                    modelLink = ModelCard.GetPredefinedModelCardByModelID("qwen2-vl:8b").ModelUri.ToString();
+                    break;
+                case "2":
+                    modelLink = ModelCard.GetPredefinedModelCardByModelID("minicpm-o").ModelUri.ToString();
+                    break;
+
+                default:
+                    modelLink = input.Trim().Trim('"').Trim('"');
+                    break;
+            }
+
+            //Loading model
+            Uri modelUri = new Uri(modelLink);
+            LM model = new LM(modelUri,
+                                    downloadingProgress: ModelDownloadingProgress,
+                                    loadingProgress: ModelLoadingProgress);
+
+            Console.Clear();
+            Summarizer summarizer = new Summarizer(model)
+            {
+                GenerateTitle = true,
+                GenerateContent = true,
+                MaxContentWords = 100,
+                Guidance = "" //set specific guidance here, if needed. IE: always summarize in French
+            };
+
+            while (true)
+            {
+                Console.Write("Enter the path to an image attachment:\n\n> ");
+                string path = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(path))
+                {
+                    break;
+                }
+
+                try
+                {
+                    Stopwatch sw = Stopwatch.StartNew();
+                    var attachment = new Attachment(path);
+                    var result = summarizer.Summarize(attachment);
+                    sw.Stop();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Title: {result.Title}");
+                    Console.WriteLine($"Summary: {result.Summary}");
+                    Console.ResetColor();
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Error: Unable to open the file at '{path}'. Details: {e.Message} Please check the file path and permissions.");
+                    Console.ResetColor();
+                }
+            }
+
+            Console.WriteLine("The program ended. Press any key to exit the application.");
+            _ = Console.ReadKey();
+        }
+    }
+}
